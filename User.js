f5d6a1a82f8059d9c7ad2866b0e597fbe0091e1b
Sumit Saurabh <sumitsaurabh112@gmail.com>
2024-07-23 18:24:15 +0530

    User.js

--- User.js
+++ User.js
@@ -1,8 +1,8 @@
+
+const {DataTypes} = require("sequelize");
 const sequelize = require("../config/db")
-const { DataTypes } = require("sequelize");
 
-const User = sequelize.define("User",{
-
+const User = sequelize.define("User", {
   username:{
     type: DataTypes.STRING,
     unique:true,
